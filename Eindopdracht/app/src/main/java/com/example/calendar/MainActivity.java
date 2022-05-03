620b38505ddc4fee21d8d29c0e036c2892c30283
Montijn <ms.bennik@student.avans.nl>
2022-05-03 14:05:57 +0200

    Added jsonParse

--- Eindopdracht/app/src/main/java/com/example/calendar/MainActivity.java
+++ Eindopdracht/app/src/main/java/com/example/calendar/MainActivity.java
@@ -4,11 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queue = Volley.newRequestQueue(this);
+        jsonParse();
+
     }
+    private void jsonParse(){
+        String url = "http://myjson.dit.upm.es/api/bins/ay9d";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("todos");
+                        for(int i = 0; i < jsonArray.length(); i++){
+                            JSONObject todo = jsonArray.getJSONObject(i);
+                            int Id = todo.getInt("Id");
+                            String description = todo.getString("description");
+                            System.out.print(description);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        queue.add(request);
+    }
+
+
 }
