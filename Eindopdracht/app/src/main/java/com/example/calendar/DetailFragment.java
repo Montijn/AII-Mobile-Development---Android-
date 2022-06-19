cb4a18852eb19352f6e23e2d39943a644485b75d
Montijn <ms.bennik@student.avans.nl>
2022-06-19 11:16:02 +0200

    added settings screen

--- Eindopdracht/app/src/main/java/com/example/calendar/DetailFragment.java
+++ Eindopdracht/app/src/main/java/com/example/calendar/DetailFragment.java
@@ -180,44 +180,5 @@ public class DetailFragment extends Fragment {
 
     }
 
-    String currentPhotoPath;
-
-//    private File createImageFile() throws IOException {
-//        // Create an image file name
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        String imageFileName = "JPEG_" + timeStamp + "_";
-//        File storageDir = this.getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-//        File image = File.createTempFile(
-//            imageFileName,  /* prefix */
-//            ".jpg",         /* suffix */
-//            storageDir      /* directory */
-//        );
-//
-//        // Save a file: path for use with ACTION_VIEW intents
-//        currentPhotoPath = image.getAbsolutePath();
-//        return image;
-//    }
-//
-//    private void dispatchTakePictureIntent() {
-//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        // Ensure that there's a camera activity to handle the intent
-//        if (takePictureIntent.resolveActivity(this.getContext().getPackageManager()) != null) {
-//            // Create the File where the photo should go
-//            File photoFile = null;
-//            try {
-//                photoFile = createImageFile();
-//            } catch (IOException ex) {
-//                // Error occurred while creating the File
-//            }
-//            // Continue only if the File was successfully created
-//            if (photoFile != null) {
-//                Uri photoURI = FileProvider.getUriForFile(this.getContext(),
-//                    "com.example.android.fileprovider",
-//                    photoFile);
-//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//            }
-//        }
-//    }
 
 }
--- Eindopdracht/app/src/main/java/com/example/calendar/MainActivity.java
+++ Eindopdracht/app/src/main/java/com/example/calendar/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.calendar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -9,9 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        applySettings();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent  = new Intent(this, SettingsActivity.class);
+        startActivityForResult(intent, 1);
 
+        return true;
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        applySettings();
+    }
+
+    void applySettings() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean bckgrnd = preferences.getBoolean("background", true);
+
+        View view = findViewById(android.R.id.content);
+        view.setBackgroundResource(bckgrnd? android.R.color.darker_gray : android.R.color.transparent);
+    }
 }
